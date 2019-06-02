b4507a9ca83ccff424345f5bd295bb03d4a50b06
evilsloth <yanq92@gmail.com>
2019-06-02 21:45:44 +0200

    - build electron dist only on tag - test auto update

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
-const { app, BrowserWindow } = require("electron");
-const path = require("path");
-const url = require("url");
+const { app, BrowserWindow } = require('electron');
+const { autoUpdater } = require('electron-updater');
+const path = require('path');
+const url = require('url');
 
 let win;
 
@@ -11,7 +12,7 @@ function createWindow() {
   win.loadURL(
     url.format({
       pathname: path.join(__dirname, `/dist/gitlab-ce-tools/index.html`),
-      protocol: "file:",
+      protocol: 'file:',
       slashes: true
     })
   );
@@ -19,22 +20,25 @@ function createWindow() {
   // The following is optional and will open the DevTools:
   // win.webContents.openDevTools()
 
-  win.on("closed", () => {
+  win.on('closed', () => {
     win = null;
   });
 }
 
-app.on("ready", createWindow);
+app.on('ready', () => {
+    createWindow();
+    autoUpdater.checkForUpdatesAndNotify();
+});
 
 // on macOS, closing the window doesn't quit the app
-app.on("window-all-closed", () => {
-  if (process.platform !== "darwin") {
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
     app.quit();
   }
 });
 
 // initialize the app's main window
-app.on("activate", () => {
+app.on('activate', () => {
   if (win === null) {
     createWindow();
   }
